adbc3854a4330b89274af5843252016aa31d8124
Han Yeongchan <yeongchann@gmail.com>
2015-06-29 23:14:49 +0900

    first commit

---
+++ mainwindow.cpp
@@ -0,0 +1,44 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+#include <QFile>
+#include <QDebug>
+#include <QByteArray>
+#include <QTimer>
+
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    QTimer *timer = new QTimer();
+    connect(timer, SIGNAL(timeout()), this, SLOT(recount()));
+    timer->start(1000);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+double MainWindow::getCpuLoadValue()
+{
+    QFile file("/proc/stat");
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return -1;
+    QByteArray arr = file.readLine(255);
+    arr.remove(0, 5);
+    QList<QByteArray> list = arr.split(' ');
+
+    double totalValue = list.at(0).toDouble() + list.at(1).toDouble() +
+            list.at(2).toDouble() + list.at(3).toDouble();
+    double cpumode = list.at(2).toDouble();
+    double systemValue = (cpumode / totalValue) * 100;
+    return systemValue;
+}
+
+void MainWindow::recount()
+{
+    ui->cpuNumber->display(this->getCpuLoadValue());
+}
